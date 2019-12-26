40ae65c5eb53273e317277ec7d0a041cfd14bfb3
Victor Amaral <vic_amaral_23@hotmail.com>
2019-12-26 13:00:01 -0300

    Adicionado layout das activities main e resultado.

---
+++ app/src/main/java/br/com/digitalhouse/caraoucoroa/view/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.digitalhouse.caraoucoroa.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.digitalhouse.caraoucoroa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
