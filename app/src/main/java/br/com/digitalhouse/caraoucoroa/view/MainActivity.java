8b0bf51e5119f8b9c71e56e5e3911caf4b7c81fc
Victor Amaral <vic_amaral_23@hotmail.com>
2020-01-11 07:34:41 -0300

    Adicionado funcionamento dos botões. Aplicativo funcionando 100%

--- app/src/main/java/br/com/digitalhouse/caraoucoroa/view/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/caraoucoroa/view/MainActivity.java
@@ -1,16 +1,37 @@
 package br.com.digitalhouse.caraoucoroa.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 import br.com.digitalhouse.caraoucoroa.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String RESULTADO_KEY = "resultado";
+    private ImageButton btnJogar;
+    private Random random = new Random();
+    private int numeroResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnJogar = findViewById(R.id.btnJogar);
+
+        btnJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numeroResultado = random.nextInt(10);
+                startActivity(new Intent(MainActivity.this, ResultadoActivity.class)
+                        .putExtra(RESULTADO_KEY, numeroResultado));
+            }
+        });
     }
 }
--- app/src/main/java/br/com/digitalhouse/caraoucoroa/view/ResultadoActivity.java
+++ app/src/main/java/br/com/digitalhouse/caraoucoroa/view/ResultadoActivity.java
@@ -1,16 +1,49 @@
 package br.com.digitalhouse.caraoucoroa.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.digitalhouse.caraoucoroa.R;
 
+import static br.com.digitalhouse.caraoucoroa.view.MainActivity.RESULTADO_KEY;
+
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imgResultado;
+    private ImageButton btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imgResultado = findViewById(R.id.imgResultado);
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        Bundle bundle = getIntent().getExtras();
+        assert bundle != null;
+        int resultado = bundle.getInt(RESULTADO_KEY);
+
+        resultado(resultado);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    public void resultado(int numero) {
+        if (numero < 5) {
+            imgResultado.setImageResource(R.drawable.moeda_cara);
+        } else {
+            imgResultado.setImageResource(R.drawable.moeda_coroa);
+        }
     }
 }
